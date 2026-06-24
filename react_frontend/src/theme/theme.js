905fdcd7fb8750c40c02e01cb5e98a6aab8ab97e
karthick <karthickssbsoft@gmail.com>
2026-06-25 01:04:34 +0530

    Initial Wealth Monitor project structure

---
+++ react_frontend/src/theme/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#007BFF",
+    },
+    secondary: {
+      main: "#2C2C2C",
+    },
+  },
+});
+
+export default theme;
